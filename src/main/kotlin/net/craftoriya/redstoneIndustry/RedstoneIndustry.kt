96e1c862cc16c5bcaf48739aba07fb4ddb24574b
Heptadecagon <besschotnov.yaroslav@gmail.com>
2026-04-12 02:06:05 +0300

    Initial commit

---
+++ src/main/kotlin/net/craftoriya/redstoneIndustry/RedstoneIndustry.kt
@@ -0,0 +1,14 @@
+package net.craftoriya.redstoneIndustry
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RedstoneIndustry : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
